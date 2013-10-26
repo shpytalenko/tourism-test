6c2c0ca705248a86164a0df9ed20df0eb99ed173
Paul <djusepo@ukr.net>
2013-10-26 03:18:48 +0300

    twitter bootstrap integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kidsland::Application.config.secret_token = '75a1c5dad5c9451a721df26020f336c9d9723b01a17250ec23788601dcf5860f5f5daba2551870b8f3580ed554e7afd698eead7cb785a1941bded336bc4f9a9c'
